da989fc89b027b281f950fa29e8e0949e999596e
Hercules Merscher <hlmerscher@gmail.com>
2022-08-14 23:39:39 +0200

    user store with linear search

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,43 @@
+import java.util.Calendar
+
+data class User(val email: String)
+
+object store {
+    private val users = mutableListOf<User>()
+
+    fun add(user: User) {
+        users.add(user)
+    }
+
+    fun find(email: String): User? {
+        for (user in users) {
+            if (user.email == email) {
+                return user
+            }
+        }
+        return null
+    }
+}
+
+
+fun timeTaken(fn: () -> Unit): Long {
+    val before = Calendar.getInstance().timeInMillis
+    fn()
+    val after = Calendar.getInstance().timeInMillis
+    return after - before
+}
+
 fun main(args: Array<String>) {
-    println("hello world")
+    for (i in 0 until 50_000_000) {
+        val user = User(email="someone@${i}")
+        store.add(user)
+    }
+
+    val milliseconds = timeTaken {
+        val notFound = store.find("not@found")?.email ?: "not found"
+        println("user ${notFound}")
+        // user not found
+    }
+    println("time taken = ${milliseconds}")
+    // time taken = 389
 }
